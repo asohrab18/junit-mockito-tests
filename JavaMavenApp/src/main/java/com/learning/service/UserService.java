02e42caa506bb14c104d304467c512092c1db2a6
Sohrab Alam <sohrab.alam86@gmail.com>
2026-09-17 16:05:30 +0530

    Java Maven APP with unit tests.

--- JavaMavenApp/src/main/java/com/learning/service/UserService.java
+++ JavaMavenApp/src/main/java/com/learning/service/UserService.java
@@ -3,6 +3,9 @@ package com.learning.service;
 public class UserService {
 
 	public String getUserType(int age) {
+		if (age <= 0) {
+			throw new IllegalArgumentException("Age cannot be zero or negative.");
+		}
 
 		if (age >= 18) {
 			return "ADULT";
--- JavaMavenApp/src/test/java/com/learning/service/UserServiceTest.java
+++ JavaMavenApp/src/test/java/com/learning/service/UserServiceTest.java
@@ -1,13 +1,53 @@
 package com.learning.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 class UserServiceTest {
 
 	@Test
-	void shouldGetAdultWhenAgeGreaterThanOrEqualTo18() {
+	void shouldThrowExeptionWhenAgeIsNegativeNumber() {
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			UserService user = new UserService();
+			user.getUserType(-12);
+		});
+
+	}
+
+	@Test
+	void shouldThrowExeptionWhenAgeIsZero() {
+
+		assertThrows(IllegalArgumentException.class, () -> {
+			UserService user = new UserService();
+			user.getUserType(0);
+		});
+
+	}
+
+	@Test
+	void shouldGetMinorWhenAgeLessThan18() {
+		UserService user = new UserService();
+		String actual = user.getUserType(17);
+		assertEquals("MINOR", actual);
+	}
+
+	@Test
+	void shouldGetAdultWhenAgeEqualTo18() {
+		// Given
+		UserService user = new UserService();
+
+		// When
+		String actual = user.getUserType(18);
+
+		// Then
+		assertEquals("ADULT", actual);
+	}
+
+	@Test
+	void shouldGetAdultWhenAgeGreaterThan18() {
 		// Given
 		UserService user = new UserService();
 
@@ -18,10 +58,4 @@ class UserServiceTest {
 		assertEquals("ADULT", actual);
 	}
 
-	@Test
-	void shouldGetMinorWhenAgeLessThan18() {
-		UserService user = new UserService();
-		String actual = user.getUserType(17);
-		assertEquals("MINOR", actual);
-	}
 }
