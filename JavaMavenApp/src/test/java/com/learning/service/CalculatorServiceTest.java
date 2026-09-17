cc850fffe2a7b16e01fbbc46175b349053b14eb1
Sohrab Alam <sohrab.alam86@gmail.com>
2026-09-17 18:09:17 +0530

    Java Maven APP with unit tests.

--- JavaMavenApp/src/test/java/com/learning/service/CalculatorServiceTest.java
+++ JavaMavenApp/src/test/java/com/learning/service/CalculatorServiceTest.java
@@ -7,34 +7,86 @@ import org.junit.jupiter.api.Test;
 
 class CalculatorServiceTest {
 
+	/**
+	 * Normal = valid everyday input. 
+	 * Boundary = input near an important limit
+	 * Negative = invalid/unacceptable input 
+	 * Exception = verify an exception is thrown
+	 */
+	
+	// Normal Case
 	@Test
 	void should_add_two_numbers() {
-		int num1 = 10, num2 = 10;
-		int expected = num1 + num2;
+		// GIVEN
+		int num1 = 10;
+		int num2 = 10;
+
+		// It is hard coded because the test should define what the correct answer is,
+		// rather than calculate the answer using the same logic.
+		int expected = 20;
 
 		CalculatorService calc = new CalculatorService();
+
+		// WHEN
 		int actual = calc.add(num1, num2);
 
+		// THEN
 		assertEquals(expected, actual);
 	}
 
+	// Normal Case
 	@Test
 	void should_divide_two_integer_numbers() {
-		int num1 = 10, num2 = 5;
-		int expected = num1 / num2;
+		// GIVEN
+		int num1 = 10;
+		int num2 = 5;
+		int expected = 2;
 
 		CalculatorService calc = new CalculatorService();
+
+		// WHEN
 		int actual = calc.divide(num1, num2);
 
+		// THEN
 		assertEquals(expected, actual);
 	}
 
+	// Exception Case
 	@Test
 	void should_throw_exception_when_divide_by_zero() {
-		assertThrows(ArithmeticException.class, () -> {
-			CalculatorService calc = new CalculatorService();
-			calc.divide(15, 0);
-		});
+		// GIVEN
+		CalculatorService calc = new CalculatorService();
+
+		// WHEN + THEN
+		assertThrows(ArithmeticException.class, () -> calc.divide(15, 0));
 
 	}
+
+	@Test
+	void should_add_negative_numbers() {
+
+		// GIVEN
+		CalculatorService calc = new CalculatorService();
+
+		// WHEN
+		int actual = calc.add(-10, -5);
+
+		// THEN
+		assertEquals(-15, actual);
+	}
+
+	// edge/boundary-type case
+	@Test
+	void should_return_zero_when_dividing_zero_by_number() {
+
+		// GIVEN
+		CalculatorService calc = new CalculatorService();
+
+		// WHEN
+		int actual = calc.divide(0, 5);
+
+		// THEN
+		assertEquals(0, actual);
+	}
+
 }
--- JavaMavenApp/src/test/java/com/learning/service/UserServiceTest.java
+++ JavaMavenApp/src/test/java/com/learning/service/UserServiceTest.java
@@ -7,30 +7,58 @@ import org.junit.jupiter.api.Test;
 
 class UserServiceTest {
 
+	/**
+	 * Normal = valid everyday input. Boundary = input near an important limit
+	 * Negative = invalid/unacceptable input Exception = verify an exception is
+	 * thrown ============================================ 17 ← just below boundary
+	 * 18 ← boundary 19 ← just above boundary
+	 **/
 	@Test
-	void should_throw_exeption_when_age_is_negative() {
+	void should_throw_exception_when_age_is_negative() {
+		// Given
+		UserService user = new UserService();
+		int age = -12;
 
+		// When & Then
 		assertThrows(IllegalArgumentException.class, () -> {
-			UserService user = new UserService();
-			user.getUserType(-12);
+			user.getUserType(age);
 		});
 
 	}
 
 	@Test
-	void should_throw_exeption_when_age_is_zero() {
+	void should_throw_exception_when_age_is_zero() {
+		// Given
+		UserService user = new UserService();
+		int age = 0;
 
+		// When & Then
 		assertThrows(IllegalArgumentException.class, () -> {
-			UserService user = new UserService();
-			user.getUserType(0);
+			user.getUserType(age);
 		});
 
+	}
+
+	@Test
+	void should_get_minor_when_age_is_one() {
+
+		UserService user = new UserService();
+
+		String actual = user.getUserType(1);
+
+		assertEquals("MINOR", actual);
 	}
 
 	@Test
 	void should_get_minor_when_age_less_than_18() {
+		// Given
 		UserService user = new UserService();
-		String actual = user.getUserType(17);
+		int age = 17;
+
+		// When
+		String actual = user.getUserType(age);
+
+		// Then
 		assertEquals("MINOR", actual);
 	}
 
@@ -38,9 +66,10 @@ class UserServiceTest {
 	void should_get_adult_when_age_is_18() {
 		// Given
 		UserService user = new UserService();
+		int age = 18;
 
 		// When
-		String actual = user.getUserType(18);
+		String actual = user.getUserType(age);
 
 		// Then
 		assertEquals("ADULT", actual);
@@ -50,12 +79,25 @@ class UserServiceTest {
 	void should_get_adult_when_age_greater_than_18() {
 		// Given
 		UserService user = new UserService();
+		int age = 19;
 
 		// When
-		String actual = user.getUserType(19);
+		String actual = user.getUserType(age);
 
 		// Then
 		assertEquals("ADULT", actual);
 	}
 
+	@Test
+	void should_get_adult_when_age_is_maximum_integer_value() {
+
+		// Given
+		UserService user = new UserService();
+
+		// When
+		String actual = user.getUserType(Integer.MAX_VALUE);
+
+		// Then
+		assertEquals("ADULT", actual);
+	}
 }
